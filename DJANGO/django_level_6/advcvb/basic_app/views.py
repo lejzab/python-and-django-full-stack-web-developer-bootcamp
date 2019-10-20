48387aad49af9e2fefc8d2dad1a694a3b6f7a946
Paweł Bogaczewicz <bogaczew@gmail.com>
2019-10-20 13:07:24 +0200

    List and Detail View pt 1

--- DJANGO/django_level_6/advcvb/basic_app/views.py
+++ DJANGO/django_level_6/advcvb/basic_app/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView,DetailView
+from . import models
 
+class SchoolListView(ListView):
+    model = models.School
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injectme'] = 'INJECTED!!!'
-
-        return context
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
