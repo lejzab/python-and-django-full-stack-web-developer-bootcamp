28391e0535239b43312297b20325c2d96ac0b930
Paweł Bogaczewicz <bogaczew@gmail.com>
2019-10-20 15:25:40 +0200

    list view pt2

--- DJANGO/django_level_6/advcvb/basic_app/views.py
+++ DJANGO/django_level_6/advcvb/basic_app/views.py
@@ -3,8 +3,10 @@ from django.views.generic import View, TemplateView, ListView,DetailView
 from . import models
 
 class SchoolListView(ListView):
+    context_object_name = 'schools'
     model = models.School
 
 class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
