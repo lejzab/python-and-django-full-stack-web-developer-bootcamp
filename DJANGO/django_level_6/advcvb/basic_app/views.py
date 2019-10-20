576397ebcbcd0f06014b45f71e6950d71a12d27d
Paweł Bogaczewicz <bogaczew@gmail.com>
2019-10-20 15:53:07 +0200

    list view fixed navbar

--- DJANGO/django_level_6/advcvb/basic_app/views.py
+++ DJANGO/django_level_6/advcvb/basic_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import View, TemplateView, ListView,DetailView
 from . import models
 
+class IndexView(TemplateView):
+    template_name='index.html'
+
 class SchoolListView(ListView):
     context_object_name = 'schools'
     model = models.School
