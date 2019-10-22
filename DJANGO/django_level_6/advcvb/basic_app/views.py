cb6fd3998142ae6fe2fec6b24797048a3d61d898
Paweł Bogaczewicz <bogaczew@gmail.com>
2019-10-22 22:23:07 +0200

    CRUD views

--- DJANGO/django_level_6/advcvb/basic_app/views.py
+++ DJANGO/django_level_6/advcvb/basic_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView,DetailView
+from django.views.generic import (View, TemplateView, ListView,DetailView,
+                                CreateView, UpdateView, DeleteView)
+from django.urls import reverse_lazy
 from . import models
 
 class IndexView(TemplateView):
@@ -13,3 +15,15 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    model = models.School
+    fields = ('name', 'principal', 'location')
+
+class SchoolUpdateView(UpdateView):
+    model = models.School
+    fields = ('name', 'principal')
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
